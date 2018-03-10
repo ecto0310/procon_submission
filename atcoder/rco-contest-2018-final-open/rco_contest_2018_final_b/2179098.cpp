bae7805e5db560b35b855ed97036db156f7dba18
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-10 08:43:29 +0000

    [add] atcoder/rco-contest-2018-final-open/rco_contest_2018_final_b/2179098.cpp

---
+++ atcoder/rco-contest-2018-final-open/rco_contest_2018_final_b/2179098.cpp
@@ -0,0 +1,19 @@
+// Date: Sat, 10 Mar 2018 08:43:29 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N,M,Q;
+  cin>> N>> M>> Q;
+  for(int i=0;i<N;i++)cout<< "a";
+  cout<< endl;
+  cout<< (float)(rand()%9)/10+1<< endl;
+  string q;
+  for(int i=0;i<Q;i++)cin>> q;
+
+  for(int i=0;i<Q;i++)cout<< rand()%N<< endl;
+  return 0;
+}
