913af06bd6f9afe57a7ac7ee1ff45c9cb4a63b6a
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-12 06:21:02 +0000

    [add] atcoder/abc087/abc087_a/2195282.cpp

---
+++ atcoder/abc087/abc087_a/2195282.cpp
@@ -0,0 +1,16 @@
+// Date: Mon, 12 Mar 2018 06:21:02 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int X,A,B;
+  cin>> X>> A>> B;
+
+  int ret=(X-A)%B;
+  cout<< ret<< endl;
+
+  return 0;
+}
