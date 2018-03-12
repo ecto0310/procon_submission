99eb93507aa70abc38186a41339407363995298b
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-12 12:31:51 +0000

    [add] atcoder/abc086/abc086_a/2196748.cpp

---
+++ atcoder/abc086/abc086_a/2196748.cpp
@@ -0,0 +1,17 @@
+// Date: Mon, 12 Mar 2018 12:31:51 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int a,b;
+  cin>> a>> b;
+
+  string ret="Even";
+  if(a*b%2)ret="Odd";
+  cout<< ret<< endl;
+
+  return 0;
+}
