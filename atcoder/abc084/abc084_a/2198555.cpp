beaf30973e810bba802f3c37ef964d25235a6a16
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-13 04:00:31 +0000

    [add] atcoder/abc084/abc084_a/2198555.cpp

---
+++ atcoder/abc084/abc084_a/2198555.cpp
@@ -0,0 +1,16 @@
+// Date: Tue, 13 Mar 2018 04:00:31 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int M;
+  cin>> M;
+
+  int ret=48-M;
+  cout<< ret<< endl;
+
+  return 0;
+}
