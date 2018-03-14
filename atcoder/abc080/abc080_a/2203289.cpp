a88f5a42b9cc4d47ed7d08f737e204183ff68457
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-14 07:01:48 +0000

    [add] atcoder/abc080/abc080_a/2203289.cpp

---
+++ atcoder/abc080/abc080_a/2203289.cpp
@@ -0,0 +1,16 @@
+// Date: Wed, 14 Mar 2018 07:01:48 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N,A,B;
+  cin>> N>> A>>B;
+
+  int ret=min(A*N,B);
+  cout<< ret<< endl;
+  
+  return 0;
+}
