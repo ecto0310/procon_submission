dde7a8d9af8724131c3d95487b3dbe360bd85d03
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-17 12:02:19 +0000

    [add] atcoder/abc091/abc091_a/2216954.cpp

---
+++ atcoder/abc091/abc091_a/2216954.cpp
@@ -0,0 +1,17 @@
+// Date: Sat, 17 Mar 2018 12:02:19 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int A,B,C;
+  cin>> A>> B>> C;
+
+  string ret="No";
+  if(A+B>=C)ret="Yes";
+  cout<< ret<< endl;
+
+  return 0;
+}
