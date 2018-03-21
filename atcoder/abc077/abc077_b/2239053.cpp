8aeb4c8cbcea026453374e2f1e2d47adb272febf
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-21 07:43:41 +0000

    [add] atcoder/abc077/abc077_b/2239053.cpp

---
+++ atcoder/abc077/abc077_b/2239053.cpp
@@ -0,0 +1,24 @@
+// Date: Wed, 21 Mar 2018 07:43:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin>> N;
+
+  int ret=0;
+  for(int i=0;;i++)
+  {
+    if(i*i>N)
+    {
+      ret=(i-1)*(i-1);
+      break;
+    }
+  }
+  cout<< ret<< endl;
+
+  return 0;
+}
