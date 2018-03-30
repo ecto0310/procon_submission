2be31cc28104f985f275af52ee1734e1f0d72200
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-03-30 11:22:41 +0000

    [add] atcoder/abc025/abc025_a/2280116.cpp

---
+++ atcoder/abc025/abc025_a/2280116.cpp
@@ -0,0 +1,16 @@
+// Date: Fri, 30 Mar 2018 11:22:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	int N;
+	cin>> S>> N;
+
+	cout<< S[(N-1)/5]<< S[(N%5+4)%5]<< endl;
+	
+	return 0;
+}
