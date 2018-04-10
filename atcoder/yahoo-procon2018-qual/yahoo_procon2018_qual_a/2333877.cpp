a71e42a16261c8a31c99b12fe75f7b9a52cbbc45
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-04-10 15:39:04 +0000

    [add] atcoder/yahoo-procon2018-qual/yahoo_procon2018_qual_a/2333877.cpp

---
+++ atcoder/yahoo-procon2018-qual/yahoo_procon2018_qual_a/2333877.cpp
@@ -0,0 +1,17 @@
+// Date: Tue, 10 Apr 2018 15:39:04 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin>> S;
+
+	string ret="NO";
+	if(S.substr(0,3)=="yah"&&S[3]==S[4])ret="YES";
+	cout<< ret<< endl;
+
+	return 0;
+}
