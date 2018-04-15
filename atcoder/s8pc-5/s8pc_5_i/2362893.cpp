7fa0d3ec835ada322bca0456691eb54fa61bb223
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-04-15 11:39:22 +0000

    [add] atcoder/s8pc-5/s8pc_5_i/2362893.cpp

---
+++ atcoder/s8pc-5/s8pc_5_i/2362893.cpp
@@ -0,0 +1,30 @@
+// Date: Sun, 15 Apr 2018 11:39:22 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int H, W, N;
+	cin >> H >> W >> N;
+	cout << 0 << endl;
+	bool rm = true;
+	string re;
+	for( int i = 0; i < H; i++ )
+	{
+		for( int j = 0; j < W - 1; j++ )
+		{
+			if( rm )cout << "R" << endl;
+			else cout << "L" << endl;
+			cin >> re;
+			if( re == "get-clear" )return 0;
+		}
+		cout << "D" << endl;
+		cin >> re;
+		if( re == "get-clear" )return 0;
+		rm = ( rm ? false : true );
+	}
+
+	return 0;
+}
