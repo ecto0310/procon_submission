bc5114e723ed8af46ccab2b1ffe8320342146a46
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-06-02 12:01:37 +0000

    [add] atcoder/bitflyer2018-qual/bitflyer2018_qual_a/2596797.cpp

---
+++ atcoder/bitflyer2018-qual/bitflyer2018_qual_a/2596797.cpp
@@ -0,0 +1,17 @@
+// Date: Sat, 02 Jun 2018 12:01:37 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int ans = b;
+	while(ans + b <= a)ans += b;
+	cout << ans << endl;
+
+	return 0;
+}
