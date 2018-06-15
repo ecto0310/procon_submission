423a81d218303f43a9e4b4308fc818d65664544c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-06-15 11:15:51 +0000

    [add] atcoder/abc072/abc072_a/2670345.cpp

---
+++ atcoder/abc072/abc072_a/2670345.cpp
@@ -0,0 +1,15 @@
+// Date: Fri, 15 Jun 2018 11:15:51 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x, t;
+	cin >> x >> t;
+	cout << (x - t > 0 ? x - t : 0) << endl;
+
+	return 0;
+}
