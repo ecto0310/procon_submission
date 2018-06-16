879a0fcc26230a18ca242fd0508484a5feca8f67
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-06-16 12:08:24 +0000

    [add] atcoder/abc100/abc100_c/2676490.cpp

---
+++ atcoder/abc100/abc100_c/2676490.cpp
@@ -0,0 +1,26 @@
+// Date: Sat, 16 Jun 2018 12:08:24 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int ans = 0;
+	for(int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		while(a % 2==0)
+		{
+			a /= 2;
+			ans++;
+		}
+	}
+	cout << ans << endl;
+
+	return 0;
+}
