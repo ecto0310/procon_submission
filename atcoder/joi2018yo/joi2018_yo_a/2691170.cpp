69f40ba1da6fb7a0f2aae17345706e19ae0050e7
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-06-16 16:47:43 +0000

    [add] atcoder/joi2018yo/joi2018_yo_a/2691170.cpp

---
+++ atcoder/joi2018yo/joi2018_yo_a/2691170.cpp
@@ -0,0 +1,15 @@
+// Date: Sat, 16 Jun 2018 16:47:43 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n, a, b, c, d;
+	cin >> n >> a >> b >> c >> d;
+	cout << min((n + a - 1) / a * b, (n + c - 1) / c * d) << endl;
+
+	return 0;
+}
