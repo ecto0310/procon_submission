d611b570e4a5e4e078d14e873b247baac7df4939
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-06 12:31:06 +0000

    [add] atcoder/abc071/abc071_a/2797374.cpp

---
+++ atcoder/abc071/abc071_a/2797374.cpp
@@ -0,0 +1,18 @@
+// Date: Fri, 06 Jul 2018 12:31:06 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x, a, b;
+	cin >> x >> a >> b;
+	if (abs(x - a) > abs(x - b))
+		cout << "B" << endl;
+	else
+		cout << "A" << endl;
+
+	return 0;
+}
