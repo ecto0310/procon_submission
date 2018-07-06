5ff92392eca71663af7f55d159f190cb439ff0a1
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-06 12:39:51 +0000

    [add] atcoder/abc071/abc071_b/2797402.cpp

---
+++ atcoder/abc071/abc071_b/2797402.cpp
@@ -0,0 +1,25 @@
+// Date: Fri, 06 Jul 2018 12:39:51 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	map<char, bool> m;
+	int size = s.size();
+	for (int i = 0; i < size; i++)
+		m[s[i]] = true;
+	for (int i = 0; i < 26; i++)
+		if (!m['a' + i])
+		{
+			cout << (char)('a' + i) << endl;
+			return 0;
+		}
+	cout << "None" << endl;
+
+	return 0;
+}
