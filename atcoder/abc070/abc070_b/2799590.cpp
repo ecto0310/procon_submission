24bafd79768e8c578732bdde27ad2f31dd25cced
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-07 06:00:07 +0000

    [add] atcoder/abc070/abc070_b/2799590.cpp

---
+++ atcoder/abc070/abc070_b/2799590.cpp
@@ -0,0 +1,15 @@
+// Date: Sat, 07 Jul 2018 06:00:07 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	cout<< max(0,min(b,d)-max(a,c))<< endl;
+
+	return 0;
+}
