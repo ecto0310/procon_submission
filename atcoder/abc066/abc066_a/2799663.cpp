b233dc9a47276410d3a4a90e652f74fc937f86f2
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-07 06:13:46 +0000

    [add] atcoder/abc066/abc066_a/2799663.cpp

---
+++ atcoder/abc066/abc066_a/2799663.cpp
@@ -0,0 +1,15 @@
+// Date: Sat, 07 Jul 2018 06:13:46 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<< a+b+c-max({a,b,c})<<endl;
+
+	return 0;
+}
