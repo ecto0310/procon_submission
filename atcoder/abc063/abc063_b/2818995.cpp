02155095cc7b7f76ab4174c98c05cffbe420d181
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-09 14:00:06 +0000

    [add] atcoder/abc063/abc063_b/2818995.cpp

---
+++ atcoder/abc063/abc063_b/2818995.cpp
@@ -0,0 +1,26 @@
+// Date: Mon, 09 Jul 2018 14:00:06 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	bool use[26] = {};
+	bool ok = true;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (use[s[i] - 'a'])
+			ok = false;
+		use[s[i] - 'a'] = true;
+	}
+	if (ok)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+
+	return 0;
+}
