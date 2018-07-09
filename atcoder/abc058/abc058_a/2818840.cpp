2621ec3e51bd2d94dfc73ad4357c5fd7c84d457a
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-09 13:24:36 +0000

    [add] atcoder/abc058/abc058_a/2818840.cpp

---
+++ atcoder/abc058/abc058_a/2818840.cpp
@@ -0,0 +1,18 @@
+// Date: Mon, 09 Jul 2018 13:24:36 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (b - a == c - b)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+	return 0;
+}
