90db1f1e9f161b25068f1864bc403443240e374a
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-10 07:50:53 +0000

    [add] atcoder/soundhound2018/soundhound2018_a/2821403.cpp

---
+++ atcoder/soundhound2018/soundhound2018_a/2821403.cpp
@@ -0,0 +1,18 @@
+// Date: Tue, 10 Jul 2018 07:50:53 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string x, y;
+	cin >> x >> y;
+	if (x[0] == 'S' && y[0] == 'H')
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+	return 0;
+}
