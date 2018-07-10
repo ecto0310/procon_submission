7d56bfebad8cd148c7cc944b53a65b23595532fc
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-10 07:47:24 +0000

    [add] atcoder/ddcc2016-qual/ddcc_2016_qual_a/2821390.cpp

---
+++ atcoder/ddcc2016-qual/ddcc_2016_qual_a/2821390.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 10 Jul 2018 07:47:24 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout <<fixed<< setprecision(7)<< (double)c / a * b << endl;
+
+	return 0;
+}
