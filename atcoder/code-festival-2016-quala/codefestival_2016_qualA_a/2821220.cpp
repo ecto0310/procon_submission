98ad98b93d1a4fcd53185625085a3595629d5fb1
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-10 07:14:31 +0000

    [add] atcoder/code-festival-2016-quala/codefestival_2016_qualA_a/2821220.cpp

---
+++ atcoder/code-festival-2016-quala/codefestival_2016_qualA_a/2821220.cpp
@@ -0,0 +1,16 @@
+// Date: Tue, 10 Jul 2018 07:14:31 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin>>s;
+	s.insert(s.begin()+4,' ');
+	cout<< s<< endl;
+
+	return 0;
+}
