01e86e9d0cf20e4ceda3d7bd4dc8317ba0d4091d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-10 07:34:53 +0000

    [add] atcoder/code-festival-2016-qualc/codefestival_2016_qualC_a/2821328.cpp

---
+++ atcoder/code-festival-2016-qualc/codefestival_2016_qualC_a/2821328.cpp
@@ -0,0 +1,26 @@
+// Date: Tue, 10 Jul 2018 07:34:53 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	bool c = false;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == 'C')
+			c = true;
+		else if (c && s[i] == 'F')
+		{
+			cout << "Yes" << endl;
+			return 0;
+		}
+	}
+	cout << "No" << endl;
+
+	return 0;
+}
