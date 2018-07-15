eabea93165fe5c66b2271c7b5853caff148411c4
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-15 14:02:29 +0000

    [add] atcoder/abc040/abc040_a/2853054.cpp

---
+++ atcoder/abc040/abc040_a/2853054.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 15 Jul 2018 14:02:29 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	cout << min(n - x, x - 1) << endl;
+
+	return 0;
+}
