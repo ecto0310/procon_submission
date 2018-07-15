e94b80d97fd7bdd3c0f7bbda9913c664a5e7900c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-15 13:12:13 +0000

    [add] atcoder/tenka1-2017-beginner/tenka1_2017_b/2852849.cpp

---
+++ atcoder/tenka1-2017-beginner/tenka1_2017_b/2852849.cpp
@@ -0,0 +1,26 @@
+// Date: Sun, 15 Jul 2018 13:12:13 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int m = 0, ans;
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (a > m)
+		{
+			ans = a + b;
+			m = a;
+		}
+	}
+	cout << ans << endl;
+
+	return 0;
+}
