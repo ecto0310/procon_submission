997a29eb63d5926c647138f36b3aaa4314bb6f55
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-15 14:07:23 +0000

    [add] atcoder/abc052/abc052_b/2853073.cpp

---
+++ atcoder/abc052/abc052_b/2853073.cpp
@@ -0,0 +1,25 @@
+// Date: Sun, 15 Jul 2018 14:07:23 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	string s;
+	cin >> n >> s;
+	int ans = 0, tmp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (s[i] == 'I')
+			tmp++;
+		else
+			tmp--;
+		ans = max(ans, tmp);
+	}
+	cout << ans << endl;
+
+	return 0;
+}
