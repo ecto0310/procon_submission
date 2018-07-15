441704924f40c00daa40c992f243b617b8174bfe
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-15 14:04:54 +0000

    [add] atcoder/abc039/abc039_a/2853064.cpp

---
+++ atcoder/abc039/abc039_a/2853064.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 15 Jul 2018 14:04:54 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << 2 * (a * b + a * c + b * c) << endl;
+
+	return 0;
+}
