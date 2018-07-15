db1a55f5fba6980db6d0aace63834a84eab11c6e
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-15 14:00:44 +0000

    [add] atcoder/abc041/abc041_a/2853047.cpp

---
+++ atcoder/abc041/abc041_a/2853047.cpp
@@ -0,0 +1,16 @@
+// Date: Sun, 15 Jul 2018 14:00:44 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	int i;
+	cin >> s >> i;
+	cout << s[i - 1] << endl;
+
+	return 0;
+}
