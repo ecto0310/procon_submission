f530839d93eeda2ea6cf96848d1bea01709c9fb9
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-16 12:30:39 +0000

    [add] atcoder/ddcc2017-qual/ddcc2017_qual_b/2856665.cpp

---
+++ atcoder/ddcc2017-qual/ddcc2017_qual_b/2856665.cpp
@@ -0,0 +1,15 @@
+// Date: Mon, 16 Jul 2018 12:30:39 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a * 1728 + b * 144 + c * 12 + d << endl;
+
+	return 0;
+}
