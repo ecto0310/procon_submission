ee1441f4060b4bf3523306bf7eedbe7e3abfdc57
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-17 12:43:38 +0000

    [add] atcoder/abc029/abc029_a/2860493.cpp

---
+++ atcoder/abc029/abc029_a/2860493.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 17 Jul 2018 12:43:38 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s << "s" << endl;
+
+	return 0;
+}
