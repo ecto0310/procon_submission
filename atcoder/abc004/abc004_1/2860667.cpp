ad6aeb167f8cada7dfbd4233dc6cc7f8a05ce122
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-17 13:18:56 +0000

    [add] atcoder/abc004/abc004_1/2860667.cpp

---
+++ atcoder/abc004/abc004_1/2860667.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 17 Jul 2018 13:18:56 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n * 2 << endl;
+
+	return 0;
+}
