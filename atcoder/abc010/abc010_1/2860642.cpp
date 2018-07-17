b4ad73e117125ae738173381cf506292ca196416
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-07-17 13:14:15 +0000

    [add] atcoder/abc010/abc010_1/2860642.cpp

---
+++ atcoder/abc010/abc010_1/2860642.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 17 Jul 2018 13:14:15 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s << "pp" << endl;
+
+	return 0;
+}
