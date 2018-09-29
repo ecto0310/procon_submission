fe54173a97fda44cb99b72c2e61151fbf8e7db49
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-09-29 04:37:01 +0000

    [add] atcoder/future-meets-you-contest-2018-open/future2018career_a/3286261.cpp

---
+++ atcoder/future-meets-you-contest-2018-open/future2018career_a/3286261.cpp
@@ -0,0 +1,11 @@
+// Date: Sat, 29 Sep 2018 04:37:01 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n,k;
+  cin>>n>>k;
+  for(int i=0;i<k;i++)cout<< 1<<" "<<2<<" "<<3<<" "<<4<<" "<< 0<< endl;
+}
