50fbd09d2662cc20eaf89fb4a8672c73f7e21c9f
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-10-06 12:00:56 +0000

    [add] atcoder/abc112/abc112_a/3341366.cpp

---
+++ atcoder/abc112/abc112_a/3341366.cpp
@@ -0,0 +1,22 @@
+// Date: Sat, 06 Oct 2018 12:00:56 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  if (t == 1)
+    cout << "Hello World" << endl;
+  else
+  {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+  }
+
+  return 0;
+}
