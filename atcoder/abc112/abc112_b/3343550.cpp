080163d52c0fdb42b2974bf75039ce897c6d317f
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-10-06 12:05:51 +0000

    [add] atcoder/abc112/abc112_b/3343550.cpp

---
+++ atcoder/abc112/abc112_b/3343550.cpp
@@ -0,0 +1,29 @@
+// Date: Sat, 06 Oct 2018 12:05:51 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int n, tl;
+  cin >> n >> tl;
+  int ans = -1, now = 10000;
+  for (int i = 0; i < n; i++)
+  {
+    int c, t;
+    cin >> c >> t;
+    if (t <= tl && c <= now)
+    {
+      ans = i + 1;
+      now = c;
+    }
+  }
+  if (ans != -1)
+    cout << now << endl;
+  else
+    cout << "TLE" << endl;
+
+  return 0;
+}
