6f3dcdf641fe0728d92db6e33816ebaf4ff84b4d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-10-14 15:37:42 +0000

    [add] atcoder/agc015/agc015_a/3410515.cpp

---
+++ atcoder/agc015/agc015_a/3410515.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 14 Oct 2018 15:37:42 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n >> a >> b;
+	cout << max((long long)0, (n - 2) * b - (n - 2) * a + 1) << endl;
+
+	return 0;
+}
