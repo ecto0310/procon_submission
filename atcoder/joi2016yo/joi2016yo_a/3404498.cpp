0d8d9ea5b2bc809fdcf43f947cf3a241f9769bfa
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-10-14 09:36:41 +0000

    [add] atcoder/joi2016yo/joi2016yo_a/3404498.cpp

---
+++ atcoder/joi2016yo/joi2016yo_a/3404498.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 14 Oct 2018 09:36:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << a + b + c + d - min({a, b, c, d}) + e + f - min(e, f) << endl;
+
+	return 0;
+}
