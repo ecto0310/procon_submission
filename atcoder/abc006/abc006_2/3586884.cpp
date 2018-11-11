af8668473a91d08fdf247f0365e29bd5d69722d2
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-11-11 16:15:55 +0000

    [add] atcoder/abc006/abc006_2/3586884.cpp

---
+++ atcoder/abc006/abc006_2/3586884.cpp
@@ -0,0 +1,19 @@
+// Date: Sun, 11 Nov 2018 16:15:55 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> tri = {0, 0, 1};
+	tri.resize(n);
+	for (int i = 3; i < n; i++)
+		tri[i] = (tri[i - 1] + tri[i - 2] + tri[i - 3]) % 10007;
+	cout << tri[n - 1] << endl;
+
+	return 0;
+}
