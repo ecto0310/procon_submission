ba9e96580f6cfb410fb0de692064ac6fd43f3c8e
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-11-14 13:53:49 +0000

    [add] atcoder/abc020/abc020_b/3600170.cpp

---
+++ atcoder/abc020/abc020_b/3600170.cpp
@@ -0,0 +1,15 @@
+// Date: Wed, 14 Nov 2018 13:53:49 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	cout << stoi(a + b) * 2 << endl;
+
+	return 0;
+}
