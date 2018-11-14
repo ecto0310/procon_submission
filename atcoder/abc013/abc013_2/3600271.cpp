fee84652ca80a3fc4a69ae47a38440cc0eafca4c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-11-14 14:14:39 +0000

    [add] atcoder/abc013/abc013_2/3600271.cpp

---
+++ atcoder/abc013/abc013_2/3600271.cpp
@@ -0,0 +1,15 @@
+// Date: Wed, 14 Nov 2018 14:14:39 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << min(abs(a - b), abs(10 - abs(a - b))) << endl;
+
+	return 0;
+}
