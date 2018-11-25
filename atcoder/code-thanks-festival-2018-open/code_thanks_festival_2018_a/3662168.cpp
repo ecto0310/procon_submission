685788918ed377f072ee79f6e023af09fd12239d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-11-25 03:01:46 +0000

    [add] atcoder/code-thanks-festival-2018-open/code_thanks_festival_2018_a/3662168.cpp

---
+++ atcoder/code-thanks-festival-2018-open/code_thanks_festival_2018_a/3662168.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 25 Nov 2018 03:01:46 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int t, a, b, c, d;
+	cin >> t >> a >> b >> c >> d;
+	cout << max({0, (a <= t ? b : 0), (c <= t ? d : 0), (a + c <= t ? b + d : 0)}) << endl;
+
+	return 0;
+}
