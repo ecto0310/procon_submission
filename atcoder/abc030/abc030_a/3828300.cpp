6bfe47a85a4970a9365a96ed7c30aeb9ae5969d7
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-12-20 12:17:33 +0000

    [add] atcoder/abc030/abc030_a/3828300.cpp

---
+++ atcoder/abc030/abc030_a/3828300.cpp
@@ -0,0 +1,15 @@
+// Date: Thu, 20 Dec 2018 12:17:33 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << ((double)b / a > (double)d / c ? "TAKAHASHI" : (double)b / a == (double)d / c ? "DRAW" : "AOKI") << endl;
+
+  return 0;
+}
