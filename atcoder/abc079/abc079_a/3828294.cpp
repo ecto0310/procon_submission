855b0b748303e6b7a59f034fb37788fe7c6c6346
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-12-20 12:16:12 +0000

    [add] atcoder/abc079/abc079_a/3828294.cpp

---
+++ atcoder/abc079/abc079_a/3828294.cpp
@@ -0,0 +1,15 @@
+// Date: Thu, 20 Dec 2018 12:16:12 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  string n;
+  cin >> n;
+  cout << ((n[0] == n[1] && n[1] == n[2] || n[1] == n[2] && n[2] == n[3]) ? "Yes" : "No") << endl;
+
+  return 0;
+}
