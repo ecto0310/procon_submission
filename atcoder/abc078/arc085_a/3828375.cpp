6d64a183190dbfffd72cf3a5be67c7b889c4cf41
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2018-12-20 12:27:49 +0000

    [add] atcoder/abc078/arc085_a/3828375.cpp

---
+++ atcoder/abc078/arc085_a/3828375.cpp
@@ -0,0 +1,15 @@
+// Date: Thu, 20 Dec 2018 12:27:49 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  cout << (1800 * m + 100 * n) * pow(2, m) << endl;
+
+  return 0;
+}
