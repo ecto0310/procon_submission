3e167f07d5121921b6352d3d292d7a502e1644f7
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-02-18 07:13:06 +0000

    [add] atcoder/nikkei2019-ex/nikkei2019ex_c/4311141.cpp

---
+++ atcoder/nikkei2019-ex/nikkei2019ex_c/4311141.cpp
@@ -0,0 +1,15 @@
+// Date: Mon, 18 Feb 2019 07:13:06 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+  cout << N % 11 << endl;
+
+  return 0;
+}
