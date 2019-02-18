9aa0a3d1c854efe61632800997301493bb23ab43
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-02-18 07:10:21 +0000

    [add] atcoder/nikkei2019-ex/nikkei2019ex_a/4311115.cpp

---
+++ atcoder/nikkei2019-ex/nikkei2019ex_a/4311115.cpp
@@ -0,0 +1,17 @@
+// Date: Mon, 18 Feb 2019 07:10:21 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  string S;
+  cin >> S;
+  int size = S.size();
+  for (int i = 1; i <= size; i++)
+    cout << i << endl;
+
+  return 0;
+}
