c17670e84f28abf1f0dcedbbe13a998c7cdc84f3
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-03-19 11:32:50 +0000

    [add] atcoder/abc072/abc072_a/4632642.cpp

---
+++ atcoder/abc072/abc072_a/4632642.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 19 Mar 2019 11:32:50 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int x, t;
+  cin >> x >> t;
+  cout << max(0, x - t) << endl;
+
+  return 0;
+}
