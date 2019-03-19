a8b7da122d555594e6ce2485dc27a01d4f100c3d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-03-19 11:31:58 +0000

    [add] atcoder/abc108/abc108_a/4632627.cpp

---
+++ atcoder/abc108/abc108_a/4632627.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 19 Mar 2019 11:31:58 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int k;
+  cin >> k;
+  cout << k / 2 * ((k + 1) / 2) << endl;
+
+  return 0;
+}
