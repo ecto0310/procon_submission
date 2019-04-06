84d3d38011a36e75fbe2e7415282b51c4cefb5e7
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-04-06 12:01:55 +0000

    [add] atcoder/abc123/abc123_a/4842973.cpp

---
+++ atcoder/abc123/abc123_a/4842973.cpp
@@ -0,0 +1,18 @@
+// Date: Sat, 06 Apr 2019 12:01:55 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c,d,e,k;
+	cin>>a>>b>>c>>d>>e>>k;
+	if(k<e-a)
+		cout<<":("<<endl;
+	else
+		cout<<"Yay!"<<endl;
+
+	return 0;
+}
