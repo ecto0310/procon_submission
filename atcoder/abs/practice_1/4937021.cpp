57026743a7d48ca49444d7e09bf335f6440e9265
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-04-13 10:43:02 +0000

    [add] atcoder/abs/practice_1/4937021.cpp

---
+++ atcoder/abs/practice_1/4937021.cpp
@@ -0,0 +1,18 @@
+// Date: Sat, 13 Apr 2019 10:43:02 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int a,b,c;
+  string s;
+  cin>> a;
+  cin>> b>> c;
+  cin>> s;
+
+  cout<< a+b+c<< " "<< s<< endl;
+
+  return 0;
+}
