9fdd3906471fbfcff9dd2795e414671bbb1e5fb2
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-04-13 10:46:14 +0000

    [add] atcoder/abs/abc088_b/4937065.cpp

---
+++ atcoder/abs/abc088_b/4937065.cpp
@@ -0,0 +1,24 @@
+// Date: Sat, 13 Apr 2019 10:46:14 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  int a[100];
+  cin>> N;
+  for(int i=0;i<N;i++)cin>> a[i];
+
+  int ret=0;
+  sort(a,a+N,greater<int>());
+  for(int i=0;i<N;i++)
+  {
+    if(i%2==0)ret+=a[i];
+    else ret-=a[i];
+  }
+  cout<< ret<< endl;
+  
+  return 0;
+}
