577dd86b57a5eb3502b2872a53cbe118c07b849e
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-04-14 11:07:14 +0000

    [add] atcoder/s8pc-6/s8pc_6_i/4973037.cpp

---
+++ atcoder/s8pc-6/s8pc_6_i/4973037.cpp
@@ -0,0 +1,29 @@
+// Date: Sun, 14 Apr 2019 11:07:14 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 h, w;
+  cin >> h >> w;
+  vector<vector<i64>> a(h, vector<i64>(w));
+  for (i64 i = 0; i < h; i++)
+    for (i64 j = 0; j < w; j++)
+      cin >> a[i][j];
+  for (i64 i = 0; i < h; i++)
+  {
+    for (i64 j = 0; j < w; j++)
+      if (i % 2 == 0)
+        cout << "#";
+      else if (j == 0)
+        cout << "#";
+      else
+        cout << ".";
+    cout << "\n";
+  }
+
+  return 0;
+}
