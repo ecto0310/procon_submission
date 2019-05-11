28b2ac111a23b20a5c9c82228f98053559b3d173
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-05-11 04:40:43 +0000

    [add] aizu_online_judge/0380/3558310.cpp

---
+++ aizu_online_judge/0380/3558310.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 11 May 2019 04:40:43 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 f;
+  cin >> f;
+  cout << (f - 30) / 2 << endl;
+  return 0;
+}
