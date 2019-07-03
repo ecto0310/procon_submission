bb39020b9ef54d442cc8f830cca905f48ec63c13
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-07-03 14:43:26 +0000

    [add] atcoder/code-festival-2017-qualc/code_festival_2017_qualc_c/6228669.cpp

---
+++ atcoder/code-festival-2017-qualc/code_festival_2017_qualc_c/6228669.cpp
@@ -0,0 +1,39 @@
+// Date: Wed, 03 Jul 2019 14:43:26 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string s;
+  cin >> s;
+  i64 l = 0, r = s.size() - 1;
+  i64 ans = 0;
+  while (0 < r - l)
+  {
+    if (s[l] == s[r])
+    {
+      l++;
+      r--;
+    }
+    else if (s[l] == 'x')
+    {
+      l++;
+      ans++;
+    }
+    else if (s[r] == 'x')
+    {
+      r--;
+      ans++;
+    }
+    else
+    {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
