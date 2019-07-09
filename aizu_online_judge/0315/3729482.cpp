e949f3b4acf29cb1cb2e379be57a48ce299361a1
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-07-09 06:35:32 +0000

    [add] aizu_online_judge/0315/3729482.cpp

---
+++ aizu_online_judge/0315/3729482.cpp
@@ -0,0 +1,14 @@
+// Date: Tue, 09 Jul 2019 06:35:32 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 p, m, c;
+  cin >> p >> m >> c;
+  cout << p + m + c << endl;
+  return 0;
+}
