ddc8aa3025b2c2d83fd6883e50c547c1137acf95
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-07-20 12:01:54 +0000

    [add] atcoder/abc134/abc134_b/6448117.cpp

---
+++ atcoder/abc134/abc134_b/6448117.cpp
@@ -0,0 +1,15 @@
+// Date: Sat, 20 Jul 2019 12:01:54 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, d;
+  cin >> n >> d;
+  d = d * 2 + 1;
+  cout << (n + d - 1) / d << endl;
+  return 0;
+}
