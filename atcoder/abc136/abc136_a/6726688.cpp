11bfb10750e6429b28ac4863b1c927dc77acfe5a
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-08-05 09:16:03 +0000

    [add] atcoder/abc136/abc136_a/6726688.cpp

---
+++ atcoder/abc136/abc136_a/6726688.cpp
@@ -0,0 +1,14 @@
+// Date: Mon, 05 Aug 2019 09:16:03 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 a, b, c;
+  cin >> a >> b >> c;
+  cout << max(0LL, c - a + b) << endl;
+  return 0;
+}
