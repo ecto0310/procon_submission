215bf8623215532dca3ba459ff7b32c680118649
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-09-11 07:09:18 +0000

    [add] aizu_online_judge/0358/3860052.cpp

---
+++ aizu_online_judge/0358/3860052.cpp
@@ -0,0 +1,17 @@
+// Date: Wed, 11 Sep 2019 07:09:18 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 m, f, b;
+  cin >> m >> f >> b;
+  if (m + f < b)
+    cout << "NA" << endl;
+  else
+    cout << max(0LL, b - m) << endl;
+  return 0;
+}
