844618c971e50123f64875f3275e1d6f469c8748
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-09-22 15:35:21 +0000

    [add] aizu_online_judge/0335/3886966.cpp

---
+++ aizu_online_judge/0335/3886966.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 22 Sep 2019 15:35:21 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 w;
+  cin >> w;
+  cout << w * 32 << endl;
+  return 0;
+}
