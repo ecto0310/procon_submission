c114eb92bd5c3617b81e3f90f0f4defdb1807a60
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-10-25 20:03:23 +0000

    [add] yukicoder/795/392861.cpp

---
+++ yukicoder/795/392861.cpp
@@ -0,0 +1,14 @@
+// Date: Fri, 25 Oct 2019 20:03:23 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, m;
+  cin >> n >> m;
+  cout << "Yes" << endl;
+  return 0;
+}
