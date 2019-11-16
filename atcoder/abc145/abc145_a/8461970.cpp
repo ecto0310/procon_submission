e4035c956293251b517b95c228d679d59031cdfb
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-11-16 12:00:27 +0000

    [add] atcoder/abc145/abc145_a/8461970.cpp

---
+++ atcoder/abc145/abc145_a/8461970.cpp
@@ -0,0 +1,13 @@
+// Date: Sat, 16 Nov 2019 12:00:27 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 r;
+  cin >> r;
+  cout << r * r << endl;
+  return 0;
+}
