f0fa77ffaad61996f9c2041a7d597fcaff6b8e38
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-11-26 06:42:29 +0000

    [add] atcoder/nikkei2019-2-qual/nikkei2019_2_qual_a/8658123.cpp

---
+++ atcoder/nikkei2019-2-qual/nikkei2019_2_qual_a/8658123.cpp
@@ -0,0 +1,16 @@
+// Date: Tue, 26 Nov 2019 06:42:29 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  if (n % 2 == 0)
+    cout << n / 2 - 1 << endl;
+  else
+    cout << n / 2 << endl;
+  return 0;
+}
