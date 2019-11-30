b49cc56bfaab7446fc66751c1f0997107f86ed71
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-11-30 11:24:45 +0000

    [add] atcoder/pakencamp-2018-day2/pakencamp_2018_day2_c/8709370.cpp

---
+++ atcoder/pakencamp-2018-day2/pakencamp_2018_day2_c/8709370.cpp
@@ -0,0 +1,33 @@
+// Date: Sat, 30 Nov 2019 11:24:45 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  vector<i64> a(n);
+  for (i64 i = 0; i < n; i++)
+    cin >> a[i];
+  for (i64 i = 1; i <= n; i++)
+  {
+    vector<i64> t(i, 0);
+    bool ok = true;
+    for (i64 j = 0; j < n; j++)
+      if (a[j] != 0)
+      {
+        if (t[j % i] == 0)
+          t[j % i] = a[j];
+        if (a[j] != t[j % i])
+          ok = false;
+      }
+    if (ok)
+    {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
