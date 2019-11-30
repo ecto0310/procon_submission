81ef6d3f4435370756b944bcdc248f93465ad476
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-11-30 11:13:15 +0000

    [add] atcoder/joi2015yo/joi2015yo_a/8709218.cpp

---
+++ atcoder/joi2015yo/joi2015yo_a/8709218.cpp
@@ -0,0 +1,13 @@
+// Date: Sat, 30 Nov 2019 11:13:15 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 a, b, c, d, p;
+  cin >> a >> b >> c >> d >> p;
+  cout << min(a * p, b + max(0LL, p - c) * d) << endl;
+  return 0;
+}
