0b4157d66b93ad16d4bde731119be0bb5f6f5525
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2019-12-22 12:04:59 +0000

    [add] atcoder/abc148/abc148_c/9060965.cpp

---
+++ atcoder/abc148/abc148_c/9060965.cpp
@@ -0,0 +1,13 @@
+// Date: Sun, 22 Dec 2019 12:04:59 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 a, b;
+  cin >> a >> b;
+  cout << a / __gcd(a, b) * b << endl;
+  return 0;
+}
