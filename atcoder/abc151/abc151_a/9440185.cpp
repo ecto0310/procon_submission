b0eea644c01586ff5ae0aef576cf6c65e74b8bf6
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-01-12 12:00:35 +0000

    [add] atcoder/abc151/abc151_a/9440185.cpp

---
+++ atcoder/abc151/abc151_a/9440185.cpp
@@ -0,0 +1,13 @@
+// Date: Sun, 12 Jan 2020 12:00:35 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string c;
+  cin >> c;
+  cout << (char)(c[0] + 1) << endl;
+  return 0;
+}
