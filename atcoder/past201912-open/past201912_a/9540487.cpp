34e53a637fe1eb2a044076cd5901015bf474033f
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-01-16 22:02:04 +0000

    [add] atcoder/past201912-open/past201912_a/9540487.cpp

---
+++ atcoder/past201912-open/past201912_a/9540487.cpp
@@ -0,0 +1,20 @@
+// Date: Thu, 16 Jan 2020 22:02:04 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string s;
+  cin >> s;
+  bool ok = true;
+  for (i64 i = 0; i < s.size(); i++)
+    if ('a' <= s[i] && s[i] <= 'z')
+      ok = false;
+  if (ok)
+    cout << stoll(s) * 2 << endl;
+  else
+    cout << "error" << endl;
+  return 0;
+}
