c97d13de6e6befe9c84248ea8f249cdd1f0f1975
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-01-19 11:50:33 +0000

    [add] atcoder/abc152/abc152_a/9596813.cpp

---
+++ atcoder/abc152/abc152_a/9596813.cpp
@@ -0,0 +1,16 @@
+// Date: Sun, 19 Jan 2020 11:50:33 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, m;
+  cin >> n >> m;
+  if (n == m)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
