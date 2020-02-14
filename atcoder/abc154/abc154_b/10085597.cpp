fb42ab33e828f92e1f77bb15f8a84583d504c8f9
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-02-14 06:45:38 +0000

    [add] atcoder/abc154/abc154_b/10085597.cpp

---
+++ atcoder/abc154/abc154_b/10085597.cpp
@@ -0,0 +1,13 @@
+// Date: Fri, 14 Feb 2020 06:45:38 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string s;
+  cin >> s;
+  cout << string(s.size(), 'x') << endl;
+  return 0;
+}
