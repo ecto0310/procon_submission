ebe909232599c6c8a525f106eb7ba33eb10dfeb6
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-02-29 08:43:29 +0000

    [add] atcoder/joi2020yo1b/joi2020_yo1b_a/10402687.cpp

---
+++ atcoder/joi2020yo1b/joi2020_yo1b_a/10402687.cpp
@@ -0,0 +1,12 @@
+// Date: Sat, 29 Feb 2020 08:43:29 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 a, b, c;
+  cin >> a >> b >> c;
+  cout << a + b + c - min({a, b, c}) << endl;
+}
