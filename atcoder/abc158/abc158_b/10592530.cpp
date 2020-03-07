13c16d6abd79ea8f674980936f49d4ddd3259cae
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-03-07 12:04:22 +0000

    [add] atcoder/abc158/abc158_b/10592530.cpp

---
+++ atcoder/abc158/abc158_b/10592530.cpp
@@ -0,0 +1,13 @@
+// Date: Sat, 07 Mar 2020 12:04:22 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 N, A, B;
+  cin >> N >> A >> B;
+  cout << N / (A + B) * A + min(A, N - N / (A + B) * (A + B)) << endl;
+  return 0;
+}
