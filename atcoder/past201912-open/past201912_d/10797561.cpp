e0e0bf294bc83b225354d616f211e6f71d9bf1fa
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-03-13 09:59:46 +0000

    [add] atcoder/past201912-open/past201912_d/10797561.cpp

---
+++ atcoder/past201912-open/past201912_d/10797561.cpp
@@ -0,0 +1,30 @@
+// Date: Fri, 13 Mar 2020 09:59:46 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  vector<i64> A(N);
+  for (i64 i = 0; i < N; i++)
+    cin >> A[i];
+  map<i64, i64> m;
+  for (i64 i = 0; i < N; i++)
+    m[A[i]]++;
+  if (m.size() == N)
+  {
+    cout << "Correct" << endl;
+    return 0;
+  }
+  for (i64 i = 1; i <= N; i++)
+    if (m[i] == 2)
+      cout << i << " ";
+  for (i64 i = 1; i <= N; i++)
+    if (m[i] == 0)
+      cout << i << endl;
+  return 0;
+}
