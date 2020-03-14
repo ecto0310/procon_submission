8631b21920d0b7433742af9f2305d92658153774
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-03-14 12:29:34 +0000

    [add] atcoder/panasonic2020/panasonic2020_d/10856353.cpp

---
+++ atcoder/panasonic2020/panasonic2020_d/10856353.cpp
@@ -0,0 +1,26 @@
+// Date: Sat, 14 Mar 2020 12:29:34 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+i64 dfs(i64 now, char t, string s)
+{
+  if (now == 0)
+  {
+    cout << s << endl;
+    return 0;
+  }
+  for (char i = 'a'; i <= t; i++)
+    dfs(now - 1, max(t, (char)(i + 1)), s + i);
+  return 0;
+}
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  dfs(N, 'a', "");
+  return 0;
+}
