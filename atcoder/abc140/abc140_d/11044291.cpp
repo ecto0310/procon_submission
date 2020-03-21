ca7235e02aaf936417f11326d53664fde329dbb4
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-03-21 10:32:23 +0000

    [add] atcoder/abc140/abc140_d/11044291.cpp

---
+++ atcoder/abc140/abc140_d/11044291.cpp
@@ -0,0 +1,23 @@
+// Date: Sat, 21 Mar 2020 10:32:23 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, K;
+  string S;
+  cin >> N >> K >> S;
+  i64 h = 0, l = 0, r = 0;
+  while (l < N)
+  {
+    while (S[l] == S[r] && r < N)
+      r++;
+    h += r - l - 1;
+    l = r;
+  }
+  cout << min(N - 1, h + 2 * K) << endl;
+  return 0;
+}
