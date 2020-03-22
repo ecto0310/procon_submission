f3947ae702652c52611361a5c797c13a9b3d3bf2
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-03-22 12:04:18 +0000

    [add] atcoder/abc159/abc159_a/11087057.cpp

---
+++ atcoder/abc159/abc159_a/11087057.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 22 Mar 2020 12:04:18 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, M;
+  cin >> N >> M;
+  cout << N * (N - 1) / 2 + M * (M - 1) / 2 << endl;
+  return 0;
+}
