cef56fdb6f3827f25a3196732324c0b1f54386f0
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-03-22 12:09:41 +0000

    [add] atcoder/abc159/abc159_c/11092329.cpp

---
+++ atcoder/abc159/abc159_c/11092329.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 22 Mar 2020 12:09:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 L;
+  cin >> L;
+  cout << fixed << setprecision(10) << pow((double)L / 3, 3) << endl;
+  return 0;
+}
