e87e9dd5eee6ee81cdc6e5a58f167d619a16c60c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-04-04 12:01:17 +0000

    [add] atcoder/abc161/abc161_a/11490111.cpp

---
+++ atcoder/abc161/abc161_a/11490111.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 04 Apr 2020 12:01:17 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 X, Y, Z;
+  cin >> X >> Y >> Z;
+  cout << Z << " " << X << " " << Y << endl;
+  return 0;
+}
