146db612b516ab5fab0265b61864a47e8e54b158
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-04-04 12:08:13 +0000

    [add] atcoder/abc161/abc161_c/11501911.cpp

---
+++ atcoder/abc161/abc161_c/11501911.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 04 Apr 2020 12:08:13 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, K;
+  cin >> N >> K;
+  cout << min(N % K, K - N % K) << endl;
+  return 0;
+}
