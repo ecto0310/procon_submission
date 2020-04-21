1dd0b403a5d92eddfc66cb49bb1edbc70c4afed6
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-04-21 14:04:36 +0000

    [add] aizu_online_judge/3137/4384081.cpp

---
+++ aizu_online_judge/3137/4384081.cpp
@@ -0,0 +1,15 @@
+// Date: Tue, 21 Apr 2020 14:04:36 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 a, b, c, d;
+  cin >> a >> b >> c >> d;
+  i64 t = a + b + c + d;
+  cout << abs(t - max({a, b, c, d}) - min({a, b, c, d}) - (max({a, b, c, d}) + min({a, b, c, d}))) << endl;
+  return 0;
+}
