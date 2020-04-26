00429db92841453324f2acb300ea875756f478d9
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-04-26 12:05:16 +0000

    [add] atcoder/abc164/abc164_a/12345988.cpp

---
+++ atcoder/abc164/abc164_a/12345988.cpp
@@ -0,0 +1,17 @@
+// Date: Sun, 26 Apr 2020 12:05:16 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 S, W;
+  cin >> S >> W;
+  if (W < S)
+    cout << "safe" << endl;
+  else
+    cout << "unsafe" << endl;
+  return 0;
+}
