0f8938a3acb195e9aa3fc0cf8f8853286f74689d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-05-03 12:00:35 +0000

    [add] atcoder/abc166/abc166_a/12706104.cpp

---
+++ atcoder/abc166/abc166_a/12706104.cpp
@@ -0,0 +1,17 @@
+// Date: Sun, 03 May 2020 12:00:35 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  string S;
+  cin >> S;
+  if (S == "ABC")
+    cout << "ARC" << endl;
+  else
+    cout << "ABC" << endl;
+  return 0;
+}
