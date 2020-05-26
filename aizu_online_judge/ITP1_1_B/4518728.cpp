be04593d62f6ea0feb81b842d7b3435c3eedefd3
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-05-26 23:50:01 +0000

    [add] aizu_online_judge/ITP1_1_B/4518728.cpp

---
+++ aizu_online_judge/ITP1_1_B/4518728.cpp
@@ -0,0 +1,14 @@
+// Date: Tue, 26 May 2020 23:50:01 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 x;
+  cin >> x;
+  cout << x * x * x << endl;
+  return 0;
+}
