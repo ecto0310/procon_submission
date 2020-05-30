f744e4c69bb5d4013c1c913f0938318cef0813d0
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-05-30 12:03:46 +0000

    [add] atcoder/nomura2020/nomura2020_b/13717390.cpp

---
+++ atcoder/nomura2020/nomura2020_b/13717390.cpp
@@ -0,0 +1,17 @@
+// Date: Sat, 30 May 2020 12:03:46 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  string T;
+  cin >> T;
+  for (char &i : T)
+    if (i == '?')
+      i = 'D';
+  cout << T << endl;
+  return 0;
+}
