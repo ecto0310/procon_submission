1caacf35992e796186d956b691f16da71b7e6e85
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-05-31 12:00:21 +0000

    [add] atcoder/abc169/abc169_a/13774318.cpp

---
+++ atcoder/abc169/abc169_a/13774318.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 31 May 2020 12:00:21 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 A, B;
+  cin >> A >> B;
+  cout << A * B << endl;
+  return 0;
+}
