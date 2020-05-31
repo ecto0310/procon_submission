c9062d20c0591d785a3394cbb848e8619afc46b1
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-05-31 12:19:19 +0000

    [add] atcoder/abc169/abc169_b/13814412.cpp

---
+++ atcoder/abc169/abc169_b/13814412.cpp
@@ -0,0 +1,28 @@
+// Date: Sun, 31 May 2020 12:19:19 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  vector<i64> A(N);
+  for (i64 i = 0; i < N; i++)
+    cin >> A[i];
+  sort(A.begin(), A.end());
+  i64 ans = A[0];
+  for (i64 i = 1; i < N; i++)
+  {
+    if (ans != 0 && (i64)1e18 / ans < A[i])
+    {
+      cout << -1 << endl;
+      return 0;
+    }
+    ans *= A[i];
+  }
+  cout << ans << endl;
+  return 0;
+}
