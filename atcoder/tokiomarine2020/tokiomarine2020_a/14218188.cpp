1555a7dca2008b3fc8433bb50250eaa304813eb4
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-06-13 12:00:38 +0000

    [add] atcoder/tokiomarine2020/tokiomarine2020_a/14218188.cpp

---
+++ atcoder/tokiomarine2020/tokiomarine2020_a/14218188.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 13 Jun 2020 12:00:38 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  string S;
+  cin >> S;
+  cout << S.substr(0, 3) << endl;
+  return 0;
+}
