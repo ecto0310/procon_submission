d9fdfc4ef0332e7083ac3d347384ac05941f2fd5
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-06-27 12:00:20 +0000

    [add] atcoder/abc172/abc172_a/14719672.cpp

---
+++ atcoder/abc172/abc172_a/14719672.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 27 Jun 2020 12:00:20 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 a;
+  cin >> a;
+  cout << a + a * a + a * a * a << endl;
+  return 0;
+}
