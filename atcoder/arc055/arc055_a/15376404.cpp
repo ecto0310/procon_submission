de7dae6459c27036df5849e4cad2141c147e6535
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-07-23 11:17:11 +0000

    [add] atcoder/arc055/arc055_a/15376404.cpp

---
+++ atcoder/arc055/arc055_a/15376404.cpp
@@ -0,0 +1,14 @@
+// Date: Thu, 23 Jul 2020 11:17:11 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  cout << "1" << string(N - 1,'0') << "7" << endl;
+  return 0;
+}
