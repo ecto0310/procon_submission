28042f5ff1226d566b13b3915295a58bcba0bc8d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-08-02 12:00:42 +0000

    [add] atcoder/abc174/abc174_a/15588358.cpp

---
+++ atcoder/abc174/abc174_a/15588358.cpp
@@ -0,0 +1,17 @@
+// Date: Sun, 02 Aug 2020 12:00:42 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 X;
+  cin >> X;
+  if (30 <= X)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
