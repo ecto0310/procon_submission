d52ee3ced97964e1b769548918fed82ff06a5903
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-08-29 07:00:09 +0000

    [add] yukicoder/9000/542871.cpp

---
+++ yukicoder/9000/542871.cpp
@@ -0,0 +1,12 @@
+// Date: Sat, 29 Aug 2020 07:00:09 +0000
+// Language: C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
+}
