453f650f92144ae2a5fa643c889f99484d4b6cf3
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-08-29 07:00:51 +0000

    [add] yukicoder/480/542872.cpp

---
+++ yukicoder/480/542872.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 29 Aug 2020 07:00:51 +0000
+// Language: C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  cout << N * (N + 1) / 2 << endl;
+  return 0;
+}
