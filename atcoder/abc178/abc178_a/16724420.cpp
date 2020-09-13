8fd984c194cd7406abee984c8825c0cdf4ce1816
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-09-13 14:07:57 +0000

    [add] atcoder/abc178/abc178_a/16724420.cpp

---
+++ atcoder/abc178/abc178_a/16724420.cpp
@@ -0,0 +1,17 @@
+// Date: Sun, 13 Sep 2020 14:07:57 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 x;
+  cin >> x;
+  if (x == 1)
+    cout << 0 << endl;
+  else
+    cout << 1 << endl;
+  return 0;
+}
