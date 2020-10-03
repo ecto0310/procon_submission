4d5d52ba443eaac9258a5f4236ee3fe06ecf81ea
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-10-03 13:29:21 +0000

    [add] atcoder/abl/abl_a/17172354.cpp

---
+++ atcoder/abl/abl_a/17172354.cpp
@@ -0,0 +1,16 @@
+// Date: Sat, 03 Oct 2020 13:29:21 +0000
+// Language: C++ (GCC 9.2.1 with AC Library)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 K;
+  cin >> K;
+  for (i64 i = 0; i < K; i++)
+    cout << "ACL";
+  cout << endl;
+  return 0;
+}
