acd7fb5ba6e290bbe516b1f3615e4317f667b21e
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-11-05 14:41:47 +0000

    [add] atcoder/abc180/abc180_a/17894817.cpp

---
+++ atcoder/abc180/abc180_a/17894817.cpp
@@ -0,0 +1,14 @@
+// Date: Thu, 05 Nov 2020 14:41:47 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, A, B;
+  cin >> N >> A >> B;
+  cout << N - A + B << endl;
+  return 0;
+}
