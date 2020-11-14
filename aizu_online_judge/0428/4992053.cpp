95efdf7f5b771fe06b860f26726aa61cd1ae337e
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-11-14 13:08:37 +0000

    [add] aizu_online_judge/0428/4992053.cpp

---
+++ aizu_online_judge/0428/4992053.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 14 Nov 2020 13:08:37 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 La, Lo;
+  cin >> La >> Lo;
+  cout << La / 3600 << " " << Lo / 3600 << endl;
+  return 0;
+}
