7011498daab69f69d6e56c7fbeab6e679474c797
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-11-22 16:41:26 +0000

    [add] atcoder/abc181/abc181_a/18353983.cpp

---
+++ atcoder/abc181/abc181_a/18353983.cpp
@@ -0,0 +1,17 @@
+// Date: Sun, 22 Nov 2020 16:41:26 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  if (N % 2 == 0)
+    cout << "White" << endl;
+  else
+    cout << "Black" << endl;
+  return 0;
+}
