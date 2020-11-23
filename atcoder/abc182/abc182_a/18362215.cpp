f984bb728415a2046fb8a4046ae7233add407f53
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-11-23 04:57:43 +0000

    [add] atcoder/abc182/abc182_a/18362215.cpp

---
+++ atcoder/abc182/abc182_a/18362215.cpp
@@ -0,0 +1,14 @@
+// Date: Mon, 23 Nov 2020 04:57:43 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 A, B;
+  cin >> A >> B;
+  cout << max(0LL, 2 * A + 100 - B) << endl;
+  return 0;
+}
