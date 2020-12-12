f0f2c6c90ad630a5dd5fe640a069c5d8bddbade8
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-12-12 03:31:55 +0000

    [add] atcoder/abc183/abc183_a/18692060.cpp

---
+++ atcoder/abc183/abc183_a/18692060.cpp
@@ -0,0 +1,17 @@
+// Date: Sat, 12 Dec 2020 03:31:55 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 x;
+  cin >> x;
+  if (0 <= x)
+    cout << x << endl;
+  else
+    cout << 0 << endl;
+  return 0;
+}
