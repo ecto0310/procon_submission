356703a07c2a4978a4b9027ad29bdc8308a3f25d
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2020-12-13 12:00:51 +0000

    [add] atcoder/abc185/abc185_a/18721973.cpp

---
+++ atcoder/abc185/abc185_a/18721973.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 13 Dec 2020 12:00:51 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 A1, A2, A3, A4;
+  cin >> A1 >> A2 >> A3 >> A4;
+  cout << min({A1, A2, A3, A4}) << endl;
+  return 0;
+}
