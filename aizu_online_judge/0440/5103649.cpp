e00b1e14b700c703be3fc4f17f1bbb93c66303fc
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-01-01 07:43:05 +0000

    [add] aizu_online_judge/0440/5103649.cpp

---
+++ aizu_online_judge/0440/5103649.cpp
@@ -0,0 +1,14 @@
+// Date: Fri, 01 Jan 2021 07:43:05 +0000
+// Language: C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 p;
+  cin >> p;
+  cout << p / 50 << endl;
+  return 0;
+}
