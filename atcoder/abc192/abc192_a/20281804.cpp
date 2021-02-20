0d387b3b4e97c5398ac8ef36cbe2c5d80eaa1f06
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-02-20 12:00:30 +0000

    [add] atcoder/abc192/abc192_a/20281804.cpp

---
+++ atcoder/abc192/abc192_a/20281804.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 20 Feb 2021 12:00:30 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main() {
+    i64 X;
+    cin >> X;
+    cout << 100 - X % 100 << endl;
+    return 0;
+}
