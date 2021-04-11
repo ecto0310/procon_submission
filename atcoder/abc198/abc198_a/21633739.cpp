885af0babc700b7c631d07876c8f6e58231e5ecb
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-04-11 12:00:24 +0000

    [add] atcoder/abc198/abc198_a/21633739.cpp

---
+++ atcoder/abc198/abc198_a/21633739.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 11 Apr 2021 12:00:24 +0000
+// Language: C++ (GCC 9.2.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main() {
+    i64 N;
+    cin >> N;
+    cout << N - 1 << endl;
+    return 0;
+}
