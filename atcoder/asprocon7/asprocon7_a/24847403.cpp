8c304bc2b4723b65688b20bebb9d14f4c36fc9cb
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-08-08 08:59:57 +0000

    [add] atcoder/asprocon7/asprocon7_a/24847403.cpp

---
+++ atcoder/asprocon7/asprocon7_a/24847403.cpp
@@ -0,0 +1,14 @@
+// Date: Sun, 08 Aug 2021 08:59:57 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  	cout << 1 << endl;
+  	cout << 1 << endl;
+    return 0;
+}
