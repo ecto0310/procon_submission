77601396db8f8aab363b6ae0685478166af811f0
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-10-23 12:01:24 +0000

    [add] atcoder/abc224/abc224_a/26753772.cpp

---
+++ atcoder/abc224/abc224_a/26753772.cpp
@@ -0,0 +1,22 @@
+// Date: Sat, 23 Oct 2021 12:01:24 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+    string S;
+    cin >> S;
+    if (S[S.size() - 1] == 'r')
+    {
+        cout << "er" << endl;
+    }
+    else
+    {
+        cout << "ist" << endl;
+    }
+    return 0;
+}
