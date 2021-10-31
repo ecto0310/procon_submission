a862a91089a43653c930df44f82beaaf85219cd1
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-10-31 06:59:13 +0000

    [add] atcoder/abc202/abc202_a/26948961.cpp

---
+++ atcoder/abc202/abc202_a/26948961.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 31 Oct 2021 06:59:13 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+    i64 a, b, c;
+    cin >> a >> b >> c;
+    cout << 21 - a - b - c << endl;
+    return 0;
+}
