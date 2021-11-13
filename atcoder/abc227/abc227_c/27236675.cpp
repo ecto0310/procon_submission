302afa0451f2b554c96a9441f2f500875c1349d2
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-11-13 13:39:40 +0000

    [add] atcoder/abc227/abc227_c/27236675.cpp

---
+++ atcoder/abc227/abc227_c/27236675.cpp
@@ -0,0 +1,23 @@
+// Date: Sat, 13 Nov 2021 13:39:40 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+    i64 N;
+    cin >> N;
+    i64 ans = 0;
+    for (i64 i = 1; i * i * i <= N; i++)
+    {
+        for (i64 j = i; 0 < N / (i * j) - j + 1; j++)
+        {
+            ans += N / (i * j) - j + 1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
