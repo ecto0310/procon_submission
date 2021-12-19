00f86370ee57ac0c194ad34720cc2a496f5dec2c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-12-19 12:00:50 +0000

    [add] atcoder/abc232/abc232_a/27983958.cpp

---
+++ atcoder/abc232/abc232_a/27983958.cpp
@@ -0,0 +1,15 @@
+// Date: Sun, 19 Dec 2021 12:00:50 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+    string S;
+    cin >> S;
+    cout << (S[0] - '0') * (S[2] - '0') << endl;
+    return 0;
+}
