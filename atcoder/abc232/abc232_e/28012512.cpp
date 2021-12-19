2e9cdac13ecbc62cdbec0edc711f1dca48eaa069
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2021-12-19 13:14:27 +0000

    [add] atcoder/abc232/abc232_e/28012512.cpp

---
+++ atcoder/abc232/abc232_e/28012512.cpp
@@ -0,0 +1,39 @@
+// Date: Sun, 19 Dec 2021 13:14:27 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+const i64 MOD = 998244353;
+
+int main()
+{
+    i64 H, W, K, x[2], y[2];
+    cin >> H >> W >> K >> x[0] >> y[0] >> x[1] >> y[1];
+    i64 dp[2][2][2] = {};
+    dp[0][(x[0] == x[1] ? 0 : 1)][(y[0] == y[1] ? 0 : 1)] = 1;
+    for (i64 i = 0; i < K; i++)
+    {
+        dp[(i + 1) & 1][0][0] = (dp[i & 1][1][0] + dp[i & 1][0][1]) % MOD;
+        dp[(i + 1) & 1][0][1] = (dp[i & 1][1][1] + dp[i & 1][0][0] * (W - 1) % MOD) % MOD;
+        dp[(i + 1) & 1][1][1] = (dp[i & 1][0][1] * (H - 1) % MOD + dp[i & 1][1][0] * (W - 1) % MOD) % MOD;
+        dp[(i + 1) & 1][1][0] = (dp[i & 1][0][0] * (H - 1) % MOD + dp[i & 1][1][1]) % MOD;
+        if (3 <= H)
+        {
+            (dp[(i + 1) & 1][1][0] += dp[i & 1][1][0] * (H - 2) % MOD) %= MOD;
+            (dp[(i + 1) & 1][1][1] += dp[i & 1][1][1] * (H - 2) % MOD) %= MOD;
+        }
+        if (3 <= W)
+        {
+            (dp[(i + 1) & 1][0][1] += dp[i & 1][0][1] * (W - 2) % MOD) %= MOD;
+            (dp[(i + 1) & 1][1][1] += dp[i & 1][1][1] * (W - 2) % MOD) %= MOD;
+        }
+    }
+    cerr << dp[K & 1][0][0] << " " << dp[K & 1][0][1] << endl;
+    cerr << dp[K & 1][1][0] << " " << dp[K & 1][1][1] << endl;
+    cerr << endl;
+    cout << dp[K & 1][0][0] << endl;
+    return 0;
+}
