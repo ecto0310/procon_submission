6124a3293da0bfa5e7452d963ee2c225ef11ac58
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2022-08-06 12:01:56 +0000

    [add] atcoder/abc263/abc263_a/33801528.cpp

---
+++ atcoder/abc263/abc263_a/33801528.cpp
@@ -0,0 +1,21 @@
+// Date: Sat, 06 Aug 2022 12:01:56 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl '\n'
+
+int main() {
+    vector<int> c(5);
+    for (int i = 0; i < 5; i++) {
+        cin >> c[i];
+    }
+    sort(c.begin(), c.end());
+    if (c[0] == c[1] && c[3] == c[4] && (c[1] == c[2] || c[2] == c[3])) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
