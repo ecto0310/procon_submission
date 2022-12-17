5397e7c073d4252e6a3da42d4c82f69d6ff4e018
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2022-12-17 12:00:41 +0000

    [add] atcoder/abc282/abc282_a/37321339.cpp

---
+++ atcoder/abc282/abc282_a/37321339.cpp
@@ -0,0 +1,16 @@
+// Date: Sat, 17 Dec 2022 12:00:41 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+int main() {
+  i64 K;
+  cin >> K;
+  for (i64 i = 0; i < K; i++) {
+    cout << (char)('A' + i);
+  }
+  cout << endl;
+  return 0;
+}
