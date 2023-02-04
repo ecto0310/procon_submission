f51faa0a3ce2308d226a29106b0906e7058847c4
ecto0310 <ecto0310@gmail.com>
2023-02-04 12:00:42 +0000

    [add] submission/atcoder/abc288/abc288_a/38594860.cpp

---
+++ atcoder/abc288/abc288_a/38594860.cpp
@@ -0,0 +1,17 @@
+// Date: Sat, 04 Feb 2023 12:00:42 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = int64_t;
+
+int main() {
+  i64 N;
+  cin >> N;
+  for (i64 i = 0; i < N; i++) {
+    i64 A, B;
+    cin >> A >> B;
+    cout << A + B << endl;
+  }
+  return 0;
+}
