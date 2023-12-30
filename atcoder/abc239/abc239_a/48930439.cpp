5606f1332abf4d5921793effd8e3f2cbda3e87e0
ecto0310 <me+github@ecto0310.com>
2023-12-30 07:53:41 +0000

    [add] submission/atcoder/abc239/abc239_a/48930439.cpp

---
+++ atcoder/abc239/abc239_a/48930439.cpp
@@ -0,0 +1,13 @@
+// Date: Sat, 30 Dec 2023 07:53:41 +0000
+// Language: C++ 20 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = int64_t;
+using namespace std;
+
+int main() {
+  ll H;
+  cin >> H;
+  cout << fixed << setprecision(7) << sqrt(H * (12800000 + H)) << endl;
+  return 0;
+}
