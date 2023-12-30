9f7032ad8348cf557792a5718714e23930b6f18f
ecto0310 <me+github@ecto0310.com>
2023-12-30 07:45:41 +0000

    [add] submission/atcoder/abc153/abc153_a/48930290.cpp

---
+++ atcoder/abc153/abc153_a/48930290.cpp
@@ -0,0 +1,13 @@
+// Date: Sat, 30 Dec 2023 07:45:41 +0000
+// Language: C++ 20 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = int64_t;
+using namespace std;
+
+int main() {
+  ll H, A;
+  cin >> H >> A;
+  cout << (H + A - 1) / A << endl;
+  return 0;
+}
