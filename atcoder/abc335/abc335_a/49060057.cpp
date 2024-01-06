3408a2189cbf08d357d1097aba83a6dfbf59c642
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-01-06 12:00:47 +0000

    [add] atcoder/abc335/abc335_a/49060057.cpp

---
+++ atcoder/abc335/abc335_a/49060057.cpp
@@ -0,0 +1,14 @@
+// Date: Sat, 06 Jan 2024 12:00:47 +0000
+// Language: C++ 20 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = int64_t;
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  S[S.size() - 1] = '4';
+  cout << S << endl;
+  return 0;
+}
