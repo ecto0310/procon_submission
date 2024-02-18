39081cc70c886940be81aff460a6ad901e91a173
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-02-18 02:28:57 +0000

    [add] atcoder/abc341/abc341_a/50405126.cpp

---
+++ atcoder/abc341/abc341_a/50405126.cpp
@@ -0,0 +1,17 @@
+// Date: Sun, 18 Feb 2024 02:28:57 +0000
+// Language: C++ 23 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll N;
+  cin >> N;
+  cout << "1";
+  for (ll i = 0; i < N; i++) {
+    cout << "01";
+  }
+  cout << endl;
+  return 0;
+}
