f9cb85361862f5c63b7f43c0fb4aa0a5df382b36
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-04-11 10:41:18 +0000

    [add] atcoder/abc082/abc082_a/52243918.cpp

---
+++ atcoder/abc082/abc082_a/52243918.cpp
@@ -0,0 +1,13 @@
+// Date: Thu, 11 Apr 2024 10:41:18 +0000
+// Language: C++ 20 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll a, b;
+  cin >> a >> b;
+  cout << (a + b + 1) / 2 << endl;
+  return 0;
+}
