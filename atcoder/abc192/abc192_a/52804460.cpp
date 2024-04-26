0c190e7ddcc2ed70bb951268b46e4cd58c7b5390
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-04-26 11:28:32 +0000

    [add] atcoder/abc192/abc192_a/52804460.cpp

---
+++ atcoder/abc192/abc192_a/52804460.cpp
@@ -0,0 +1,12 @@
+// Date: Fri, 26 Apr 2024 11:28:32 +0000
+// Language: C++ 23 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll X;
+  cin >> X;
+  cout << 100 - X % 100 << endl;
+}
