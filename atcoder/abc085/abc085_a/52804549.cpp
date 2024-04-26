a5fdb9102a325e972dddc18ceeec2ed82fb03abd
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-04-26 11:32:35 +0000

    [add] atcoder/abc085/abc085_a/52804549.cpp

---
+++ atcoder/abc085/abc085_a/52804549.cpp
@@ -0,0 +1,13 @@
+// Date: Fri, 26 Apr 2024 11:32:35 +0000
+// Language: C++ 23 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  S[3] = '8';
+  cout << S << endl;
+}
