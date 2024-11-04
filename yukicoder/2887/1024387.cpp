21533d0bb90938754876e7d02e052bf48b873345
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-11-04 22:58:46 +0000

    [add] yukicoder/2887/1024387.cpp

---
+++ yukicoder/2887/1024387.cpp
@@ -0,0 +1,13 @@
+// Date: Mon, 04 Nov 2024 22:58:46 +0000
+// Language: C++17
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll N;
+  cin >> N;
+  cout << string(N, 'a') << endl;
+  return 0;
+}
