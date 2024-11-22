cb1d7c359d25731a24ebf8b49a408bdd7c875a4f
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-11-22 15:19:54 +0000

    [add] atcoder/abc381/abc381_e/60093835.cpp

---
+++ atcoder/abc381/abc381_e/60093835.cpp
@@ -0,0 +1,70 @@
+// Date: Fri, 22 Nov 2024 15:19:54 +0000
+// Language: C++ 23 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll N, Q;
+  cin >> N >> Q;
+  string S;
+  cin >> S;
+  vector<ll> c1(N + 1), c2(N + 1);
+  vector<ll> sidx;
+  for (ll i = 0; i < N; i++) {
+    if (S[i] == '1') {
+      c1[i + 1]++;
+    } else if (S[i] == '2') {
+      c2[i + 1]++;
+    } else {
+      sidx.push_back(i);
+    }
+    c1[i + 1] += c1[i];
+    c2[i + 1] += c2[i];
+  }
+  for (ll i : c1) {
+    cerr << i << " ";
+  }
+  cerr << endl;
+  for (ll i : c2) {
+    cerr << i << " ";
+  }
+  cerr << endl;
+  for (ll i : sidx) {
+    cerr << i << " ";
+  }
+  cerr << endl;
+  for (ll _ = 0; _ < Q; _++) {
+    ll L, R;
+    cin >> L >> R;
+    L--;
+    R--;
+    ll lidx = lower_bound(sidx.begin(), sidx.end(), L) - sidx.begin();
+    ll ridx = lower_bound(sidx.begin(), sidx.end(), R + 1) - sidx.begin();
+    if (lidx == ridx) {
+      cout << 0 << endl;
+      continue;
+    }
+    ridx--;
+    ll ans = 0;
+    while (lidx <= ridx) {
+      ll mid = (lidx + ridx) / 2;
+      ll mv = sidx[mid];
+      ll l1 = c1[mv + 1] - c1[L];
+      ll r2 = c2[R + 1] - c2[mv];
+      // cerr << lidx << " " << ridx << " " << l1 << " " << c1[mv + 1] << " "
+      //      << c1[L] << " " << r2 << " " << c2[R + 1] << " " << c2[mv] <<
+      //      endl;
+      ans = max(ans, 2 * min(l1, r2) + 1);
+      if (l1 < r2) {
+        lidx = mid + 1;
+      } else {
+        ridx = mid - 1;
+      }
+    }
+    cout << ans << endl;
+  }
+
+  return 0;
+}
