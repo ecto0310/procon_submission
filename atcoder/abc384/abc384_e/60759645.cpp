152feeebe899f037dd3111ac0523db0b1713a30c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-12-14 13:01:52 +0000

    [add] atcoder/abc384/abc384_e/60759645.cpp

---
+++ atcoder/abc384/abc384_e/60759645.cpp
@@ -0,0 +1,61 @@
+// Date: Sat, 14 Dec 2024 13:01:52 +0000
+// Language: C++ 23 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+ll dh[4] = {1, -1, 0, 0};
+ll dw[4] = {0, 0, 1, -1};
+
+int main() {
+  ll H, W, X;
+  cin >> H >> W >> X;
+  ll P, Q;
+  cin >> P >> Q;
+  P--;
+  Q--;
+  vector<vector<ll>> S(H, vector<ll>(W));
+  for (vector<ll> &i : S) {
+    for (ll &j : i) {
+      cin >> j;
+    }
+  }
+  vector<vector<bool>> use(H, vector<bool>(W));
+  auto is_gird = [&](ll h, ll w) {
+    return (0 <= h && h < H && 0 <= w && w < W);
+  };
+  priority_queue<pair<ll, pair<ll, ll>>, vector<pair<ll, pair<ll, ll>>>,
+                 greater<pair<ll, pair<ll, ll>>>>
+      que;
+  ll p = S[P][Q];
+  use[P][Q] = true;
+  for (ll i = 0; i < 4; i++) {
+    ll ah = P + dh[i];
+    ll aw = Q + dw[i];
+    if (!is_gird(ah, aw)) {
+      continue;
+    }
+    use[ah][aw] = true;
+    que.push({S[ah][aw], {ah, aw}});
+  }
+  while (!que.empty()) {
+    auto [s, pos] = que.top();
+    auto [nh, nw] = pos;
+    que.pop();
+    if (S[nh][nw] < (p + X - 1) / X) {
+      p += S[nh][nw];
+      for (ll i = 0; i < 4; i++) {
+        ll ah = nh + dh[i];
+        ll aw = nw + dw[i];
+        if (!is_gird(ah, aw) || use[ah][aw]) {
+          continue;
+        }
+        use[ah][aw] = true;
+        que.push({S[ah][aw], {ah, aw}});
+      }
+    }
+  }
+  cout << p << endl;
+  return 0;
+}
