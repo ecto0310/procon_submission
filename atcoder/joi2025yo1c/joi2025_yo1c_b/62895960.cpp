8de7e8bf23b214669bedec2737b1a256670c5c86
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2025-02-18 12:01:10 +0000

    [add] atcoder/joi2025yo1c/joi2025_yo1c_b/62895960.cpp

---
+++ atcoder/joi2025yo1c/joi2025_yo1c_b/62895960.cpp
@@ -0,0 +1,17 @@
+// Date: Tue, 18 Feb 2025 12:01:10 +0000
+// Language: C++ 20 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+    ll A,B,C;
+    cin>>A>>B>>C;
+    if(A+B+C<= 21){
+        cout<<1<<endl;
+    }else{
+        cout<< 0<<endl;
+    }
+  return 0;
+}
