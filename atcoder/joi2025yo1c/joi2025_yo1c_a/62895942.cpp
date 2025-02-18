3f01ebddc47be69420564b29eeff7862f774fc0a
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2025-02-18 12:00:34 +0000

    [add] atcoder/joi2025yo1c/joi2025_yo1c_a/62895942.cpp

---
+++ atcoder/joi2025yo1c/joi2025_yo1c_a/62895942.cpp
@@ -0,0 +1,13 @@
+// Date: Tue, 18 Feb 2025 12:00:34 +0000
+// Language: C++ 20 (gcc 12.2)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll A, B;
+  cin >> A >> B;
+  cout << A * 1000 + B * 10000 << endl;
+  return 0;
+}
