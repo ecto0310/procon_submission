cce52ff0a115919a66a8ba32d1836a09f4b0e90c
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2025-09-19 20:49:21 +0000

    [add] atcoder/ahc054/ahc054_a/69443379.py

---
+++ atcoder/ahc054/ahc054_a/69443379.py
@@ -0,0 +1,17 @@
+// Date: Fri, 19 Sep 2025 20:49:21 +0000
+// Language: Python (CPython 3.11.4)
+N, ti, tj = map(int, input().split())
+b = [input() for _ in range(N)]
+revealed = [[False] * N for _ in range(N)]
+while True:
+    pi, pj = map(int, input().split())
+    parts = input().split()
+    n = int(parts[0])
+    xy = list(map(int, parts[1:]))
+    for k in range(n):
+        x = xy[2 * k]
+        y = xy[2 * k + 1]
+        revealed[x][y] = True
+    if pi == ti and pj == tj:
+        break
+    print(0, flush=True)
