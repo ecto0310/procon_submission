f39ecbc047e0755d86354092181aa6955556911a
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2026-02-20 11:13:11 +0000

    [add] atcoder/abc142/abc142_a/73447107.cpp

---
+++ atcoder/abc142/abc142_a/73447107.cpp
@@ -0,0 +1,13 @@
+// Date: Fri, 20 Feb 2026 11:13:11 +0000
+// Language: C++23 (GCC 15.2.0)
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+  ll N;
+  cin >> N;
+  cout << fixed << setprecision(7) << (double)(N - N / 2) / N << endl;
+  return 0;
+}
